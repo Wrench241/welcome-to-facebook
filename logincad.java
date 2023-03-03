54887930a04ca71ffd66528d1d3f377875b0b518
Octavius <121876628+Wrench241@users.noreply.github.com>
2023-03-03 04:30:29 -0300

    Add files via upload

---
+++ logincad.java
@@ -0,0 +1,26 @@
+public class logincad {
+    private String login;
+    private String senha;
+    public String prints;
+
+    public String[] dados =new String[6];
+
+   public void login12(String login21){
+        this.login = login21;
+
+    }
+    public void senha12 (String senha21){
+        this.senha = senha21;
+    }
+   public String getSenha(){
+    return this.senha;
+   }
+   public String getLogin(){
+    return this.login;
+   }
+   public void printS(String print){
+    System.out.print(prints = print);
+   }
+}
+
+
